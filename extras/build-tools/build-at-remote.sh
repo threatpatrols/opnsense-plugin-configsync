bf4cb5548d27759a1b9af6641c96c4edacb1805d
Nicholas de Jong <contact@nicholasdejong.com>
2018-08-06 17:36:15 +0800

    improve build tool for use with opnsense-plugins repo

--- extras/build-tools/build-at-remote.sh
+++ extras/build-tools/build-at-remote.sh
@@ -1,37 +1,56 @@
 #!/bin/bash
 
+set -e
+
 remote_host=${1}
 remote_user=root
 
-plugin_path='net-mgmt/configsync'
-local_base_path=$(realpath $(dirname $(realpath $0))/../../../opnsense-plugins)
-remote_base_path=/root/opnsense-plugins
+opnsense_plugins_repo_path=$(realpath $(dirname $(realpath $0))/../../../opnsense-plugins)
+configsync_plugin_repo_path=$(realpath $(dirname $(realpath $0))/../../)
+configsync_opnsense_plugins_subpath='net-mgmt/configsync'
+remote_base_path='/root/opnsense-plugins'
 
 if [ -z ${remote_host} ]; then
     echo 'usage: '$0' <host-address>'
     exit 1
 fi
 
-# push the code to the remote FreeBSD system
+# echo some helpful output
+echo ""
+echo "remote:                               ${remote_user}@${remote_host}"
+echo "remote_base_path:                     ${remote_base_path}"
+echo "opnsense_plugins_repo_path:           ${opnsense_plugins_repo_path}"
+echo "configsync_plugin_repo_path:          ${configsync_plugin_repo_path}"
+echo "configsync_opnsense_plugins_subpath:  ${configsync_opnsense_plugins_subpath}"
+echo ""
+
+
+# copy into place the material to be injected into the repo
+rm -Rf ${opnsense_plugins_repo_path}/${configsync_opnsense_plugins_subpath}
+mkdir ${opnsense_plugins_repo_path}/${configsync_opnsense_plugins_subpath}
+rsync -ra ${configsync_plugin_repo_path}/src/ ${opnsense_plugins_repo_path}/${configsync_opnsense_plugins_subpath}/src/
+rsync -a ${configsync_plugin_repo_path}/LICENSE ${opnsense_plugins_repo_path}/${configsync_opnsense_plugins_subpath}/LICENSE
+rsync -a ${configsync_plugin_repo_path}/Makefile ${opnsense_plugins_repo_path}/${configsync_opnsense_plugins_subpath}/Makefile
+rsync -a ${configsync_plugin_repo_path}/pkg-descr ${opnsense_plugins_repo_path}/${configsync_opnsense_plugins_subpath}/pkg-descr
+rsync -a ${configsync_plugin_repo_path}/README.md ${opnsense_plugins_repo_path}/${configsync_opnsense_plugins_subpath}/README.md
+
+# push the local opnsense_plugins_repo to the remote FreeBSD system
 rsync \
-    -rv \
+    --recursive \
     --copy-links \
     --delete \
-    --exclude .git \
-    --exclude work \
-    --exclude package \
-    ${local_base_path}/ \
-    $remote_user@$remote_host:${remote_base_path}
+    ${opnsense_plugins_repo_path}/ \
+    ${remote_user}@${remote_host}:${remote_base_path}
 
 ## do the build
-ssh $remote_user@$remote_host "cd ${remote_base_path}/${plugin_path}; rm -Rf work; make clean; make package"
+ssh ${remote_user}@${remote_host} "cd ${remote_base_path}/${configsync_opnsense_plugins_subpath}; make clean; make package"
 
 ## pull the .txz packages back
 rsync \
     --ignore-existing \
-    $remote_user@$remote_host:${remote_base_path}/${plugin_path}/work/pkg/*.txz \
-    $(realpath $(dirname $(realpath $0))/../../package)
+    ${remote_user}@${remote_host}:${remote_base_path}/${configsync_opnsense_plugins_subpath}/work/pkg/*.txz \
+    ${configsync_plugin_repo_path}/package
 
 ## re-roll the SHA256SUMS
-cd $(realpath $(dirname $(realpath $0))/../../package)
+cd ${configsync_plugin_repo_path}/package
 sha256sum *.txz > SHA256SUMS
