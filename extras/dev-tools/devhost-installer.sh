0f96c32b697a17f6f2f83e53db5795ae3ee0e58d
Nicholas de Jong <contact@nicholasdejong.com>
2018-08-10 20:46:43 +0800

    a more explicit installer and uninstaller

--- extras/dev-tools/devhost-installer.sh
+++ extras/dev-tools/devhost-installer.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+set -e
+
 remote=${1}
 action=${2}
 
 if [ -z ${remote} ] || [ -z ${action} ]; then
-    echo 'usage: '$0' <host-address> <install|remove>'
+    echo 'usage: '$0' <host-address> <install|uninstall>'
     exit 1
 fi
 
@@ -12,7 +14,7 @@ if ! [[ ${remote} = *"@"* ]]; then
     remote="root@${remote}"
 fi
 
-local_base_path=$(realpath $(dirname $0)/../)
+local_base_path=$(realpath $(dirname $0)/../../)
 local_opnsense_base_path="${local_base_path}/src/opnsense"
 remote_opnsense_base_path="/usr/local/opnsense"
 
@@ -28,48 +30,47 @@ deploy_file()
     rsync -a --no-o --no-g "${local_opnsense_path}/${src_file}" "${remote_opnsense_path}/${dst_file}"
 }
 
-remove_file()
+dev_install()
 {
-    local dst_file=${1}
-    ssh $remote "rm \"${remote_opnsense_base_path}/${dst_file}\""
+    # deploy_files
+    cd ${local_opnsense_path}
+    for filename_find in $(find . -type f -not -name *.pyc); do
+        filename=$(echo $filename_find | sed 's/^..//')
+        deploy_file ${filename} ${filename}
+    done
+
+    # deploy_service
+    rsync -a --no-o --no-g "${local_opnsense_path}/../etc/rc.d/configsync" "${remote}:/usr/local/etc/rc.d/configsync"
+    rsync -a --no-o --no-g "${local_opnsense_path}/../etc/inc/plugins.inc.d/configsync.inc" "${remote}:/usr/local/etc/inc/plugins.inc.d/configsync.inc"
+
+    # deploy_www
+    rsync -a --no-o --no-g "${local_opnsense_path}/../www/diag_logs_configsync.php" "${remote}:/usr/local/www/diag_logs_configsync.php"
+
+    ssh $remote "configctl template reload VerbNetworks/ConfigSync"
+    ssh $remote "/usr/local/etc/rc.configure_plugins"
+
 }
 
-configure_plugins()
+dev_uninstall()
 {
+    ssh $remote "configctl configsync stop"
+    ssh $remote "rm -Rf /usr/local/opnsense/mvc/app/models/VerbNetworks"
+    ssh $remote "rm -Rf /usr/local/opnsense/mvc/app/views/VerbNetworks"
+    ssh $remote "rm -Rf /usr/local/opnsense/mvc/app/controllers/VerbNetworks"
+    ssh $remote "rm -Rf /usr/local/opnsense/scripts/VerbNetworks"
+    ssh $remote "rm -Rf /usr/local/opnsense/service/templates/VerbNetworks"
+    ssh $remote "rm -Rf /var/db/configsync; rm -Rf /var/cache/configsync;"
+
+    ssh $remote "rm -f /usr/local/etc/rc.d/configsync"
+    ssh $remote "rm -f /usr/local/etc/inc/plugins.inc.d/configsync.inc"
+    ssh $remote "rm -f /usr/local/www/diag_logs_configsync.php"
+    ssh $remote "rm -f /usr/local/opnsense/service/conf/actions.d/actions_configsync.conf"
     ssh $remote "/usr/local/etc/rc.configure_plugins"
 }
 
-reload_config()
-{
-    ssh $remote "configctl template reload VerbNetworks/ConfigSync"
-}
 
-deploy_service()
-{
-    rsync -a --no-o --no-g "${local_opnsense_path}/../etc/rc.d/configsync" "${remote}:/usr/local/etc/rc.d/configsync"
-    rsync -a --no-o --no-g "${local_opnsense_path}/../etc/inc/plugins.inc.d/configsync.inc" "${remote}:/usr/local/etc/inc/plugins.inc.d/configsync.inc"
-}
-
-deploy_www()
-{
-    rsync -a --no-o --no-g "${local_opnsense_path}/../www/diag_logs_configsync.php" "${remote}:/usr/local/www/diag_logs_configsync.php"
-}
-
-
-cd ${local_opnsense_path}
-for filename_find in $(find . -type f -not -name *.pyc); do
-    filename=$(echo $filename_find | sed 's/^..//')
-    if [ $action = "remove" ]; then
-        remove_file ${filename}
-    else
-        deploy_file ${filename} ${filename}
-    fi
-done
-
-configure_plugins
-reload_config
-
-if [ $action != "remove" ]; then
-    deploy_service
-    deploy_www
+if [ $action = "install" ]; then
+    dev_install
+elif [ $action = "uninstall" ]; then
+    dev_uninstall
 fi
